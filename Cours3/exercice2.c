7f9f98e1b1aa74b919d780922a91393cb60984fa
Stéphane Letz <letz@grame.fr>
2025-10-20 13:50:24 +0200

    Add exercice2, to be completed.

---
+++ Cours3/exercice2.c
@@ -0,0 +1,44 @@
+ 
+#include <stdio.h>
+
+/*
+ Control structure: using FOR loop to manipulate arrays
+*/
+    
+int main()
+{
+    /* Define an array with explicit values */
+    float table1[10] = { 1.f, 23.f, 67.f, 100.f, 56.f, 29.f, 0.f, 7.f, 234.f, 10.f };
+    
+    int i = 0;
+    
+    // TODO: compute the sum of all values on the array
+    for (i = 0; i < 10; i = i+1) {
+        // TODO
+    }
+    
+    printf("Sum = %f\n", // TODO);
+    
+    /* Define an array with explicit values */
+    float table2[10] = { 1.f, 23.f, 67.f, 100.f, 56.f, 29.f, 0.f, 7.f, 234.f, 10.f };
+    
+    // TODO: compute the mean of all values on the array
+    for (i = 0; i < 10; i = i+1) {
+        // TODO
+    }
+    
+    printf("Mean = %f\n", // TODO);
+   
+    /* Define an array with explicit values */
+    float table3[10] = { 1.f, 23.f, 67.f, 100.f, 56.f, 29.f, 0.f, 7.f, 234.f, 10.f };
+    
+    // TODO: compute the maximun and minimum of all values on the array
+    for (i = 0; i < 10; i = i+1) {
+        // TODO (using 'if' contruct)
+    }
+    
+    printf("Minimum = %f\n", // TODO);
+    printf("Maximum = %f\n", // TODO);
+    
+    return 0;
+}
